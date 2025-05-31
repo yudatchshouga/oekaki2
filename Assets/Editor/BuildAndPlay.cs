8d9b99207a066ebc233ebe50352dcfea6bed3307
yudatch <yudatchshouga@gmail.com>
2025-05-31 19:51:07 +0900

    エディタ拡張

--- Assets/Editor/BuildAndPlay.cs
+++ Assets/Editor/BuildAndPlay.cs
@@ -3,6 +3,7 @@ using System.IO;
 using System.Linq;
 using UnityEditor;
 using UnityEditor.Build.Reporting;
+using UnityEditor.ShortcutManagement;
 using UnityEngine;
 
 public class BuildAndPlay
@@ -16,15 +17,24 @@ public class BuildAndPlay
             .Select(s => s.path)
             .ToArray();
 
-        // ビルド
+        // ビルド  
         BuildReport report = BuildPipeline.BuildPlayer(scenes, exePath, BuildTarget.StandaloneWindows64, BuildOptions.Development);
         if (report.summary.result != BuildResult.Succeeded)
         {
             UnityEngine.Debug.LogError("ビルド失敗");
             return;
         }
+        BuildSummary summary = report.summary;
 
-        // ビルドされた.exeを起動（1人目）
+        UnityEngine.Debug.Log("Total time: " + summary.totalTime.TotalSeconds + " seconds");
+        UnityEngine.Debug.Log("Total size: " + (summary.totalSize / 1024 / 1024) + " MB");
+
+        foreach (var step in report.steps)
+        {
+            UnityEngine.Debug.Log($"Step: {step.name} | Time: {step.duration.TotalSeconds:F2} seconds");
+        }
+
+        // ビルドされた.exeを起動（1人目）  
         if (File.Exists(exePath))
         {
             Process.Start(exePath);
@@ -34,7 +44,16 @@ public class BuildAndPlay
             UnityEngine.Debug.LogError("ビルドされた .exe が見つかりません");
         }
 
-        // エディタ上で Play 開始（2人目）
-        EditorApplication.isPlaying = true;
+        // エディタ上で Play 開始（2人目）  
+        EditorApplication.delayCall += () =>
+        {
+            EditorApplication.isPlaying = true;
+        };
+    }
+
+    [Shortcut("Tools/BuildAndPlay", KeyCode.B, ShortcutModifiers.Action | ShortcutModifiers.Shift)]
+    public static void BuildAndLaunchAndPlayShortcut()
+    {
+        BuildAndLaunchAndPlay();
     }
 }
